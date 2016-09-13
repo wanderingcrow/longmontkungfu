c120bb6ce82d59b35ace17c45e267f9dfe868ae7
Perry Kibler <pk@unleashed.io>
2016-09-13 14:23:01 -0600

    working prototype

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -1,1 +1,18 @@
-console.log('boo');
+jQuery(document).ready(function($){
+	var isLateralNavAnimating = false;
+
+	//open/close lateral navigation
+	$('.nav-trigger').on('click', function(event){
+		event.preventDefault();
+		//stop if nav animation is running
+		if( !isLateralNavAnimating ) {
+			if($(this).parents('.csstransitions').length > 0 ) isLateralNavAnimating = true;
+
+			$('body').toggleClass('navigation-is-open');
+			$('.main-nav').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				//animation is over
+				isLateralNavAnimating = false;
+			});
+		}
+	});
+});
