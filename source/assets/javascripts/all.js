b9b448982af34dec20d21bc83c2ac58d04d3f758
Perry Kibler <pk@unleashed.io>
2016-09-22 19:53:34 -0600

    home page is looking HAWT

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -1,7 +1,10 @@
 jQuery(document).ready(function($){
+
+	// navigation
+
 	var isLateralNavAnimating = false;
 
-	//open/close lateral navigation
+	// open/close lateral navigation
 	$('.nav-trigger').on('click', function(event){
 		event.preventDefault();
 		//stop if nav animation is running
@@ -10,9 +13,29 @@ jQuery(document).ready(function($){
 
 			$('body').toggleClass('navigation-is-open');
 			$('.main-nav').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-				//animation is over
+				// animation is over
 				isLateralNavAnimating = false;
 			});
 		}
 	});
+
+
+
+	// slick sliders
+  $('.slick-slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+		arrows: false
+	});
+
+
+
+	// random home page hero
+	var random;
+	random = Math.floor(Math.random() * $('.hero-bkgrnd').length);
+	$('.hero-bkgrnd').hide().eq(random).show();
+
 });
